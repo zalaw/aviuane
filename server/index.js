a5272819221f7eb0645af75ae266358b3e67a130
zalaw <keep_razvy@yahoo.com>
2023-02-28 17:54:55 +0200

    origin changed to *

--- server/index.js
+++ server/index.js
@@ -14,12 +14,13 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://aviuane.onrender.com",
-      "https://aviuane.up.railway.app/",
-    ],
+    // origin: [
+    //   "http://localhost:3000",
+    //   "http://localhost:3001",
+    //   "https://aviuane.onrender.com",
+    //   "https://aviuane.up.railway.app/",
+    // ],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
